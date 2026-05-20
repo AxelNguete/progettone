b432f7c3e0c5a02f96695c81421287a2c87d4177
nguetezangue.axel <axel.nguete@edu.iisleviponti.it>
2026-05-20 09:14:34 +0200

    p

--- src/main/java/levi/progettone/MainApplication.java
+++ src/main/java/levi/progettone/MainApplication.java
@@ -7,16 +7,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
-TODO - funzionalità ancora da implementare:
-Catena di mangiate: dopo una mangiata, se la stessa pedina può mangiarne
-un'altra il turno deve continuare invece di passare all'avversario.
-
-Evidenziare le mosse valide: quando si seleziona una pedina, colorare in
-verde le caselle raggiungibili così il giocatore sa subito dove può muoversi.
-*/
-
-
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
